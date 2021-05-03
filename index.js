5f13b02e5ada93d481ee57da0f37b995de897140
Dmitry Dudin <dudintv@gmail.com>
2021-05-03 15:03:57 +0200

    chore: try to use middleware functions

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 import express from 'express'
 import path from 'path'
+import {requestTime, logger} from './middlewares.js'
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('static', 'index.html'));
-})
-
-app.get('/features', (req, res) => {
-  res.sendFile(path.resolve('static', 'features.html'));
-})
+app.use(express.static(path.resolve('static')));
+app.use(requestTime);
+app.use(logger);
 
 app.get('/download', (req, res) => {
+  console.log(req.requestTime);
   res.download(path.resolve('static', 'index.html'));
-})
+});
 
 app.listen(PORT, () => {
   console.log(`START on ${PORT} port!`);

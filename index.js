6806030d4d67ef8f01d203aee2dc5b18a7f15709
Dmitry Dudin <dudintv@gmail.com>
2021-05-03 14:50:25 +0200

    feat: add static html page

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import path from 'path'
 
+const PORT = process.env.PORT ?? 3000;
 const app = express();
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('static', 'index.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`START on ${PORT} port!`);
+});

45a9809e13401af15672968c11b679852e8ae3ea
Dmitry Dudin <dudintv@gmail.com>
2021-05-03 15:49:01 +0200

    chore: try to use ejs

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ import {requestTime, logger} from './middlewares.js'
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
-app.use(express.static(path.resolve('static')));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('ejs'));
+console.log(app.get('views'));
+
 app.use(requestTime);
 app.use(logger);
 
-app.get('/download', (req, res) => {
-  console.log(req.requestTime);
-  res.download(path.resolve('static', 'index.html'));
-});
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'My main EJS data title'
+  });
+})
+
+// app.use(express.static(path.resolve('static')));
+
+// app.get('/download', (req, res) => {
+//   console.log(req.requestTime);
+//   res.download(path.resolve('static', 'index.html'));
+// });
 
 app.listen(PORT, () => {
   console.log(`START on ${PORT} port!`);
